d30f890396d6e3f867e028387124d83268203922
Midhun Elangovan <midhunabimanyue@gmail.com>
2026-07-09 21:14:03 +0530

    Initial commit, includes ping controller, basic application properties settings.

---
+++ src/test/java/com/io/sas/SasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.sas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
